dacce81274d8c56a9c102ae9f74b256c4abd0e79
Ruan Destro <ruandstr@gmail.com>
2025-02-13 22:26:32 -0300

    Vamos

---
+++ controller/js/defaut.js
@@ -0,0 +1,27 @@
+let index = 0;
+const images = document.querySelectorAll('.carousel img');
+
+function showSlide() {
+    const carousel = document.getElementById('carousel');
+    const width = document.querySelector('.carousel img').clientWidth;
+    carousel.style.transform = `translateX(${-index * width}px)`;
+    
+    images.forEach(img => img.classList.remove('active'));
+    images[index].classList.add('active');
+}
+
+function prevSlide() {
+    index = (index > 0) ? index - 1 : images.length - 1;
+    showSlide();
+}
+
+function nextSlide() {
+    index = (index < images.length - 1) ? index + 1 : 0;
+    showSlide();
+}
+
+function autoSlide() {
+    nextSlide();
+    setTimeout(autoSlide, (1000)*5);
+}
+setTimeout(autoSlide, (1000)*5);
